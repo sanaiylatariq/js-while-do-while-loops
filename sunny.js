b043ec0197f150c7afdccde812a7af99c2e65b6c
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-14 12:31:06 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,117 @@
+// // while loops
+// while (2==2&&2!=2) {
+//     console.log(`yes equal hay`);
+//     break
+// }
+// let why = 0
+// while (why<=10) {
+//     console.log(`value of why is ${why}`);
+//    why =  why + 3
+// }
+// let array1 = ['batman', 'sunny', 'kato']
+// arr = 0
+// while (arr<array1.length) {
+//     console.log(`value is ${array1[arr] }`);
+// arr = arr + 1
+// }
+// // do loop
+// arr1 = 0
+// let array2 = ['sana', 'sana2','sana3']
+
+// do {
+//     console.log(`values are ${array2[arr1]}`);arr1 = arr1 + 1
+// } while (arr1<array2.length);
+
+
+// let array4 = [0,1,2,3,4,]
+// while (array4.includes(0)) {
+//     console.log(`length of array4 is 1`);
+//     break
+// }
+// let sunny = 5
+// while (sunny==5) {
+//      sunny ++
+//     console.log(`sunny ki values`);
+// }
+// let dani = 25
+// while (dani >=5) {
+//     console.log(`dani ki values ${dani}`);
+//     dani = dani - 5
+//     break
+// }
+
+// let score = 0
+// while (score<10) {
+//     console.log(`score is greater than ${score}`);
+//     score = score + 5
+// break
+// }
+// let arrays = [0,1,2,3,"sanaiyla",]
+// while (arrays.includes(`sanaiyla`)) {
+//     console.log(`array has 1`);
+//     break
+// }
+// do {
+//     console.log(`i know while do loops`);
+// } while (score==0);
+// arr = 0
+// myar = ["sanaiyla","sunny","kato",]
+// while (arr<myar.length) {
+//     console.log(`values are ${myar[arr]}`);
+// arr = arr + 1
+// }
+// for (let index = 0; index < 10; index++) {
+//     if (index>2) {
+//         console.log(`index values greater than 2 detected`);
+//     break
+//     }
+    
+// console.log(`values of ${index}`);   
+// index = index + 1
+// }
+// let kato = 0
+// do {
+//     console.log(`values are ${kato}`);
+//     kato++
+//     break
+// } while (kato=10);
+
+
+// while (kato=10) {
+//     console.log(`values are ${kato}`);
+//     kato++
+//     break
+// }
+// for (let index = 0; index <10; index++) {
+// console.log(`outer loop`);
+// for (let i = 0; i < 10; i++) {
+// console.log(index+`*`+i+`=`+index*i);    
+
+// if (index => 8) {
+//     console.log(`value of outer loop as 8 dtected`);
+//     break
+// }
+
+
+// }    
+// }
+let array1 = [0,1,2,3,4,5,]
+
+for (let index = 0; index < 10; index++) {
+console.log(`outer lopop values of ${index}`);
+if (index==5) {
+    console.log(`index values of 5 detected`);
+    break
+}    
+
+}
+for (let index = 0; index < array1.length; index++) {
+console.log(array1.includes(2));  
+console.log(Array.isArray(array1));
+console.log(Array.of(index));
+console.log(array1.length);
+console.log(array1.flat(Infinity));
+break  
+}
+
+
